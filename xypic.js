19d06e102553f2db2a1c31a6e08eec89e8d7ec08
anosatsuk124 <58501479+anosatsuk124@users.noreply.github.com>
2019-12-11 19:33:23 +0900

    Update xypic.js

--- xypic.js
+++ xypic.js
@@ -16769,4 +16769,4 @@ MathJax.Hub.Register.StartupHook("SVG Xy-pic Require",function () {
   MathJax.Hub.Startup.signal.Post("SVG Xy-pic Ready");
 });
 
-MathJax.Ajax.loadComplete("https://anosatsuk124.github.io/xypic.js");
+MathJax.Ajax.loadComplete("https://anosatsuk124.github.io/philos_psych/xypic.js");
