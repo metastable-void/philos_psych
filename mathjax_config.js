43e2474c1224eb58afa451cdba2c032c666b94ec
anosatsuk124 <58501479+anosatsuk124@users.noreply.github.com>
2019-12-13 14:22:30 +0900

    Update mathjax_config.js

--- mathjax_config.js
+++ mathjax_config.js
@@ -2,6 +2,7 @@ MathJax.Hub.Config({tex2jax:{inlineMath:[['\$','\$'],['\\(','\\)']],
 processEscapes:true},
 TeX: { extensions: ["https://anosatsuk124.github.io/philos_psych/xypic.js", "mhchem.js"],
 Macros: {
+      aar: '{\\ar@{-}[#1], 1}',
       gfrac: ['\\genfrac{}{}{0px}{0}{}{#1}, 1'],
       abs: ['\\lvert #1 \\rvert', 1]
 }
