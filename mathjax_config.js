2bc217dda0ad50e2c348e701b13d78415bded2f8
anosatsuk124 <58501479+anosatsuk124@users.noreply.github.com>
2019-12-13 14:04:58 +0900

    Update and rename mathjax_macro.js to mathjax_config.js

---
+++ mathjax_config.js
@@ -0,0 +1,6 @@
+MathJax.Hub.Config({tex2jax:{inlineMath:[['\$','\$'],['\\(','\\)']],
+processEscapes:true},
+TeX: { extensions: ["https://anosatsuk124.github.io/philos_psych/xypic.js", "mhchem.js"]},
+CommonHTML: {matchFontHeight:false}});
+
+MathJax.Ajax.loadComplete("https://anosatsuk124.github.io/philos_psych/mathjax_config.js");
--- mathjax_macro.js
+++
@@ -1,8 +0,0 @@
-MathJax.Hub.Config({
- TeX:{
-  Macros:{
-   b: ['{\\mathbf{#1}}',1]
-  }
- }
-});
-MathJax.Ajax.loadComplete("https://anosatsuk124.github.io/philos_psych/mathjax_macro.js");
