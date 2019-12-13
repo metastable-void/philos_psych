421620d8f128c9a0ff9b65cb6f5e9df322e13fd2
anosatsuk124 <58501479+anosatsuk124@users.noreply.github.com>
2019-12-13 14:40:16 +0900

    Update mathjax_config.js

--- mathjax_config.js
+++ mathjax_config.js
@@ -3,8 +3,9 @@ processEscapes:true},
 TeX: { extensions: ["https://anosatsuk124.github.io/philos_psych/xypic.js", "mhchem.js"],
 Macros: {
       aar: '{\\ar@{-}[#1], 1}',
-      gfrac: ['{\\genfrac{ }{ }{0px}{0}{ }{#1}}', 1],
+      gf: ['{\\genfrac{ }{ }{0px}{0}{ }{#1}}', 1],
       abs: ['\\lvert #1 \\rvert', 1]
+      f: ['{\\displaystyle\\frac{#1}{#2}}', 2]
 }
 },
 CommonHTML: {matchFontHeight:false}});
