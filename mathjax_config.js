eb0126d6dedce4a4b60a61f178a8cb2273715f5e
anosatsuk124 <58501479+anosatsuk124@users.noreply.github.com>
2019-12-14 16:11:22 +0900

    Update mathjax_config.js

--- mathjax_config.js
+++ mathjax_config.js
@@ -1,6 +1,6 @@
 MathJax.Hub.Config({tex2jax:{inlineMath: [ ['$','$'], ['\\(','\\)'] ], displayMath: [ ['$$','$$'], ['\[','\]'] ],
 processEscapes:true},
-TeX: { extensions: ["https://anosatsuk124.github.io/philos_psych/xypic.js", "mhchem.js", "https://anosatsuk124.github.io/philos_psych/chemfig3.js", "http://tikzjax.com/v1/tikzjax.js"],
+TeX: { extensions: ["https://anosatsuk124.github.io/philos_psych/xypic.js", "mhchem.js", "https://anosatsuk124.github.io/philos_psych/chemfig3.js", "https://anosatsuk124.github.io/philos_psych/tikzjax.js"],
 Macros: {
       line: '{\\ar@{-}[#1], 1}',
       gf: ['{\\genfrac{ }{ }{0px}{0}{ }{#1}}', 1],
