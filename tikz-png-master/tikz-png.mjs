d1167f554574ad8b706a365c3cbf14fe4cad0e7d
anosatsuk124 <58501479+anosatsuk124@users.noreply.github.com>
2019-12-15 18:53:29 +0900

    Update tikz-png.mjs

--- tikz-png-master/tikz-png.mjs
+++ tikz-png-master/tikz-png.mjs
@@ -34,21 +34,15 @@ e_input.addEventListener ('change', ev => void ((async ev => {
 	const img = new Image;
 	const svg_src = 'data:image/svg+xml,' + encodeURIComponent (svg_text);
 	svg_src.src='';
+	const svg_src = img.src = 'data:image/svg+xml,' + encodeURIComponent (svg_text);
 	await new Promise (res => {
 		img.onload = ev => res (ev);
 	});
-	
-	console.log (img);
-	
-	const canvas = document.createElement ('canvas');
-	canvas.width = img.width;
-	canvas.height = img.height;
-	const ctx = canvas.getContext ('2d');
-	
-	ctx.clearRect (0, 0, canvas.width, canvas.height);
+@@ -49,6 +48,6 @@ e_input.addEventListener ('change', ev => void ((async ev => {
 	ctx.drawImage (img, 0, 0);
-	
+
 	e_download.href = canvas.toDataURL ('img/png');
 	e_img.src = e_download.href;
+	e_img.src = svg_src;
 	e_output.textContent = '';
 }) (ev)));
